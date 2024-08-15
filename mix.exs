56cbf42f42d815e0d12f9cd46f149dec21629086
Diogo Scudelletti <diogo@scudelletti.com>
2024-08-15 09:38:00 +0200

    Bump version

--- mix.exs
+++ mix.exs
@@ -1,7 +1,7 @@
 defmodule OffBroadwayAmqp10.MixProject do
   use Mix.Project
 
-  @version "0.1.3"
+  @version "0.1.4"
   @description "An AMQP 1.0 connector for Broadway"
   @source_url "https://github.com/highmobility/off_broadway_amqp10"
 
